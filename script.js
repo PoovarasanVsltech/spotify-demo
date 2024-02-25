b06b8aa09c42a18a7f6c59f7457bf0f4211d5d59
PoovarasanVsltech <147792954+PoovarasanVsltech@users.noreply.github.com>
2024-02-25 08:12:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+function closeNavbar() {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var collapse = document.querySelector('.navbar-collapse');
+    if (collapse.classList.contains('show')) {
+        navbarToggler.click();
+    }
+}
